be9e09ce9b4ee4b218be85265a42e4ae511889be
Marcin Szczepanski <mszczepanski@atlassian.com>
2016-07-12 12:07:24 +1000

    Support deploying to Atlassian 3rd Party repo

---
+++ README-ATLASSIAN.md
@@ -0,0 +1,26 @@
+# Atlassian README
+
+While this is a fork of the [offical maintenance branch of Jackson 1](https://github.com/FasterXML/jackson-1), it is
+only forked so that we can deploy a release with all the latest fixes to the Atlassian 3rd Party Maven repository. As
+currently there is no official plan to release a 1.9.14, and we require some of the fixes that have been backported from
+the Jackson 2.x release.
+
+The repository already contains Ant scripts for deploying to Maven using the Ant Maven Tasks, however these are deprecated
+and don't appear to work correctly with maven.atlassian.com. To get around this a deployment script has been created that
+uses Maven and the `deploy:deploy-file` Mojo directly.
+
+## Deploying to your local .m2 repo
+
+This still works using the Ant Maven build:
+
+* run `ant maven.local.deploy`
+
+## Deploying to maven.atlassian.com
+
+* Set the `IMPL_VERSION` property in `build.xml` (eg. `<property name="IMPL_VERSION" value="1.9.13-atlassian-1" />`) 
+* Build a release using `ant all`
+* To deploy a snapshot run `bin/atlassian-deploy.sh snapshot`
+* To deploy a release run `bin/atlassian-deploy.sh release`
+
+The deployment script will verify that all required files are present before doing the deploy using 
+`mvn deploy:deploy-file`.
---
+++ bin/atlassian-deploy.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+set -xe
+
+SCRIPT_DIR=${0%/*}
+DIST_DIR=$SCRIPT_DIR/../dist
+MAVEN_BASE_URL=https://maven.atlassian.com
+VERSION_FILE=$SCRIPT_DIR/../build/VERSION.txt
+
+if [ ! -f $VERSION_FILE ]; then
+    echo "Version file $VERSION_FILE not found - did you build?";
+    exit 1
+fi
+
+VERSION=`cat $VERSION_FILE`
+
+case $1 in
+"snapshot")
+    POM_DIR=$SCRIPT_DIR/../build/snapshot
+    SUFFIX="-SNAPSHOT"
+    MAVEN_URL=$MAVEN_BASE_URL/3rdparty-snapshot
+    MAVEN_REPO=atlassian-3rdparty-snapshot
+    ;;
+"release")
+    POM_DIR=$SCRIPT_DIR/../dist
+    SUFFIX=
+    MAVEN_URL=$MAVEN_BASE_URL/3rdparty
+    MAVEN_REPO=atlassian-3rdparty
+    ;;
+*)
+    echo "Usage: $0 [snapshot|release]"
+    exit 1
+esac
+
+shopt -s nullglob
+POM_FILES=($POM_DIR/*-$VERSION$SUFFIX.pom)
+if [ ${#POM_FILES[@]} -eq 0 ]; then
+    echo "No POM files found in $POM_DIR for version $VERSION$SUFFIX!";
+    exit 1
+fi
+
+set +x
+# set parameter to TRUE to actually deploy, otherwise this does a dry run (ie. check if all files exist)
+function deploy() {
+    for POM in ${POM_FILES[@]}
+    do
+        BASE=$(basename $POM $SUFFIX.pom)
+        ARTIFACT_ID=$(basename $POM -$VERSION$SUFFIX.pom)
+
+        if [ "$1" = false ]; then
+            echo "Checking for dist and source JAR for POM $POM"
+        fi
+
+        JAR=$DIST_DIR/$BASE.jar
+        if [ ! -f $JAR ]; then
+            echo "JAR file $JAR not found!"
+            exit 1
+        fi
+
+        SOURCE_JAR=$DIST_DIR/$BASE-sources.jar
+        if [ ! -f $SOURCE_JAR ]; then
+            echo "Source JAR file $SOURCE_JAR not found!"
+            exit 1
+        fi
+
+        if [ "$1" = true ]; then
+            echo "Deploying $ARTIFACT_ID:"
+            echo "    POM: $POM"
+            echo "    Binary: $JAR"
+            echo "    Source: $SOURCE_JAR"
+            mvn deploy:deploy-file -Durl=$MAVEN_URL \
+                   -DrepositoryId=$MAVEN_REPO \
+                   -Dfile=$JAR \
+                   -DgroupId=org.codehaus.jackson \
+                   -DartifactId=$ARTIFACT_ID \
+                   -Dversion=$VERSION$SUFFIX \
+                   -Dpackaging=jar \
+                   -DpomFile=$POM \
+                   -Dsources=$SOURCE_JAR
+        fi
+
+    done
+}
+
+deploy false
+if [ ! "$2" = "--dry-run" ]; then
+    echo "Files checked OK, starting deploy..."
+    deploy true
+fi
